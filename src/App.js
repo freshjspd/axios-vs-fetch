192e833f71e16b5b2909a87054e0de6b2a415853
freshjspd <freshjs.pd@gmail.com>
2023-05-06 10:48:22 +0300

    errors

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ const options = {
   seed: 'pd2023'
 };
 
+const configAxios = {
+  onUploadProgress: (e) => console.log(e),
+  onDownloadProgress: (e) => console.log(e),
+  timeout: 1000,
+}
+
 function loadWithFetch({results, seed, page}){
   fetch(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)
-    .then((response) => response.json())
+    .then((response) => {
+      if(!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
     .then((data) => {console.dir(data.results)})
     .catch((error) => console.error(error));
 }
 
 function loadWithAxios({results, seed, page}){
-  axios.get(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)
+  axios.get(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`, configAxios)
     .then((response) => {console.log('AXIOS', response); console.dir(response.data.results)})
-    .catch((error) => console.error(error));
+    .catch((error) => console.error(error.message));
 }
 
 function App() {
-  loadWithFetch(options);
+  //loadWithFetch(options);
   loadWithAxios(options);
   return (
     <>
