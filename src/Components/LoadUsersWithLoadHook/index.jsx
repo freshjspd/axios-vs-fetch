e09efcb4fd130b6b7ad093261bbca7091ac01491
freshjspd <freshjs.pd@gmail.com>
2023-05-06 13:37:36 +0300

    load users component with custom hook

--- src/Components/LoadUsersWithLoadHook/index.jsx
+++ src/Components/LoadUsersWithLoadHook/index.jsx
@@ -2,12 +2,25 @@ import React, {useState, useEffect} from 'react';
 import useDataLoad from '../../hooks/useDataLoad';
 
 export default function LoadUsersWithLoadHook(props) {
-  const {data, isLoading, isError} = useDataLoad();
+  const [currentPage, setCurrentPage] = useState(1);
+  const {data, isLoading, isError} = useDataLoad(currentPage);
+
+  const prevPage = () => {
+    if(currentPage > 1) setCurrentPage(currentPage -1);
+  }
+  const nextPage = () => {
+    setCurrentPage(currentPage +1);
+  }
+
+  useEffect(()=>{},[currentPage])
+  
 
   return (
     <>
         {isError && <p>Error load</p>}
         {isLoading && <p>Load...</p>}
+        <button onClick={prevPage}>prev page</button> 
+        <button onClick={nextPage}>next page</button> 
         <ol>
             {data.map((u,i) => (
                 <li key={i}>{JSON.stringify(u)}</li>
--- src/hooks/useDataLoad.js
+++ src/hooks/useDataLoad.js
@@ -1,14 +1,14 @@
 import { useState, useEffect} from "react";
 import api from '../settings/api';
 
-function useDataLoad (){
+function useDataLoad (currentPage){
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
 
     useEffect(()=>{
         setIsLoading(true);
-        (api.loadWithAxios())
+        (api.loadWithAxios({page: currentPage}))
             .then(data => setData(data))
             .catch((error) => setIsError(true))
             .finally(()=>{setIsLoading(false)});
